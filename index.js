f72c798f9562e9cc0b86d2db27b7272761c3ae7a
hazelnut6 <hazelpachoco6@gmail.com>
2025-05-18 11:46:01 +0800

    Static files

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const homeRoute = require('./routes/homeRoute');
 const itemRoute = require('./routes/itemRoute');
 
 index.set('view engine', 'ejs');
+index.use(express.static('public'));
 index.use(express.urlencoded({ extended: true }));
 
 index.use('/', homeRoute);
